7dc693d94da069f706802c9ce3080ce1748e1ec2
Wyatt Greenway <th317erd@gmail.com>
2016-09-13 19:07:53 -0700

    Updates

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 	require('./lib/base')(root);
 	require('./lib/deferred')(root);
 
+	root.data = require('./lib/data')();
 	root.utils = require('./lib/utils')();
 	root.events = require('./lib/events')();
 
